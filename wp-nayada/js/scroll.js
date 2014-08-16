a9d26e42d14032719476b98d2b941e6b1906864f
crazyyy <crazyyy@gmail.com>
2014-08-16 17:49:42 +0300

    add main theme, ready to work

--- wp-nayada/js/scroll.js
+++ wp-nayada/js/scroll.js
@@ -1,26 +1,3 @@
-// DOM Ready
-$(function() {
-	
-	// SVG fallback
-	// toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script#update
-	if (!Modernizr.svg) {
-		var imgs = document.getElementsByTagName('img');
-		var dotSVG = /.*\.svg$/;
-		for (var i = 0; i != imgs.length; ++i) {
-			if(imgs[i].src.match(dotSVG)) {
-				imgs[i].src = imgs[i].src.slice(0, -3) + "png";
-			}
-		}
-	}
-
-});
-// tabs 
-// https://github.com/aarondo/tabulous.js
-$(document).ready(function ($) {
-    $('#tabs').tabulous();  
-});
-// menu scroll 
-//https://github.com/alvarotrigo/Front-end--jQuery--CSS-/tree/master/staticMenu
 ;$(document).ready(function(){
 	
 	//by default, the static menu is hidden
