666424a692e3f80140a550773469861a44eb4642
crazyyy <crazyyy@gmail.com>
2014-08-17 00:29:18 +0300

    add aboutus

---
+++ wp-nayada/templates-aboutus.php
@@ -0,0 +1,39 @@
+<?php query_posts("p=30"); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<article class="aboutus">
+	<div class="inner">
+		<h6><?php the_title(); ?></h6>
+			<h5><?php the_field( "subtitle" ); ?></h5>
+			<a href="<?php the_field( "sertificate_big" ); ?>" class="sertificate"><img src="<?php the_field( "sertificate_small" ); ?>" alt=""><span></span></a>
+			<h4 class="avards"><?php the_field( "awards_text" ); ?></h4>
+			<?php if( have_rows('awards') ): ?>
+			<ul class="avards clearfix">
+				<?php while( have_rows('awards') ): the_row(); 
+	    			$awards_img_small = get_sub_field('awards_img_small');
+	    			$awards_img_big = get_sub_field('awards_img_big');
+		        ?>
+				<li><a href="<?php echo $awards_img_big; ?>"><img src="<?php echo $awards_img_small; ?>" alt=""></a></li>
+	    		<?php endwhile; ?>
+	    	</ul><!-- avards -->
+	    	<?php endif; ?>
+		<h4 class="comments"><?php the_field( "clients_title" ); ?></h4>
+		<?php if( have_rows('clients_response') ): ?>
+		<ul class="clearfix client-response">
+			<?php while( have_rows('clients_response') ): the_row(); 
+    			$responde_content = get_sub_field('responde_content');
+    			$client_name = get_sub_field('client_name');
+	        ?>
+			<li>
+				<p><?php echo $responde_content; ?></p>
+				<span class="name"><?php echo $client_name; ?></span>
+			</li>
+			<?php endwhile; ?>
+		</ul><!-- client-response -->
+		<?php endif; ?>
+	</div><!-- inner -->
+</article>  <!-- aboutus -->  
+
+
+<?php endwhile; endif; ?>
+<?php wp_reset_query(); ?>
