e99632b465d655c9c2a303824a4177ed9bf1f124
crazyyy <crazyyy@gmail.com>
2014-08-22 16:58:07 +0300

    rebl

--- wp-nayada/templates-about-water.php
+++ wp-nayada/templates-about-water.php
@@ -1,8 +1,8 @@
 <?php query_posts("p=20"); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<article id="about-water" class="about-water">
-	<h2><?php the_title(); ?></h2>
-	<div id="tabs">
+<article class="about-water">
+	<h2 id="about-water"><?php the_title(); ?></h2>
+	<div  id="tabs">
 		<ul class="tab-header">
 	        <li><a href="#tabs-1" title=""><?php the_field( "tab_name_1" ); ?></a></li>
 	        <li><a href="#tabs-2" title=""><?php the_field( "tab_name_2" ); ?></a></li>
